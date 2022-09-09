49d523a0547bb998931feef4bd1264aa14b75a24
shujashah83 <104627775+shujashah83@users.noreply.github.com>
2022-09-09 02:14:07 -0700

    Added Click Handeler to all input Buttons

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -37,6 +37,7 @@ class Calculator extends Component {
         }
         break;
     }
+    console.log(this.state);
   };
 
   render() {
@@ -48,25 +49,25 @@ class Calculator extends Component {
           { result }
         </article>
         <section className="calc-input">
-          <button type="submit" className="calc-btn frstRow Operator init">0</button>
-          <button type="submit" className="calc-btn frstRow Operator">.</button>
-          <button type="submit" className="calc-btn frstRow calci">=</button>
-          <button type="submit" className="calc-btn secondRow Operator">1</button>
-          <button type="submit" className="calc-btn secondRow Operator">2</button>
-          <button type="submit" className="calc-btn secondRow Operator">3</button>
-          <button type="submit" className="calc-btn secondRow calci">+</button>
-          <button type="submit" className="calc-btn thirdRow Operator">4</button>
-          <button type="submit" className="calc-btn thirdRow Operator">5</button>
-          <button type="submit" className="calc-btn thirdRow Operator">6</button>
-          <button type="submit" className="calc-btn thirdRow calci">-</button>
-          <button type="submit" className="calc-btn forthRow Operator">7</button>
-          <button type="submit" className="calc-btn forthRow Operator">8</button>
-          <button type="submit" className="calc-btn forthRow Operator">9</button>
-          <button type="submit" className="calc-btn forthRow calci">x</button>
-          <button type="submit" className="calc-btn fifthRow Operator">AC</button>
-          <button type="submit" className="calc-btn fifthRow Operator">+/-</button>
-          <button type="submit" className="calc-btn fifthRow Operator">%</button>
-          <button type="submit" className="calc-btn fifthRow calci">^</button>
+          <button type="submit" onClick={this.clickHandler} className="calc-btn frstRow Operator init">0</button>
+          <button type="submit" onClick={this.clickHandler} className="calc-btn frstRow Operator">.</button>
+          <button type="submit" onClick={this.clickHandler} className="calc-btn frstRow calci">=</button>
+          <button type="submit" onClick={this.clickHandler} className="calc-btn secondRow Operator">1</button>
+          <button type="submit" onClick={this.clickHandler} className="calc-btn secondRow Operator">2</button>
+          <button type="submit" onClick={this.clickHandler} className="calc-btn secondRow Operator">3</button>
+          <button type="submit" onClick={this.clickHandler} className="calc-btn secondRow calci">+</button>
+          <button type="submit" onClick={this.clickHandler} className="calc-btn thirdRow Operator">4</button>
+          <button type="submit" onClick={this.clickHandler} className="calc-btn thirdRow Operator">5</button>
+          <button type="submit" onClick={this.clickHandler} className="calc-btn thirdRow Operator">6</button>
+          <button type="submit" onClick={this.clickHandler} className="calc-btn thirdRow calci">-</button>
+          <button type="submit" onClick={this.clickHandler} className="calc-btn forthRow Operator">7</button>
+          <button type="submit" onClick={this.clickHandler} className="calc-btn forthRow Operator">8</button>
+          <button type="submit" onClick={this.clickHandler} className="calc-btn forthRow Operator">9</button>
+          <button type="submit" onClick={this.clickHandler} className="calc-btn forthRow calci">x</button>
+          <button type="submit" onClick={this.clickHandler} className="calc-btn fifthRow Operator">AC</button>
+          <button type="submit" onClick={this.clickHandler} className="calc-btn fifthRow Operator">+/-</button>
+          <button type="submit" onClick={this.clickHandler} className="calc-btn fifthRow Operator">%</button>
+          <button type="submit" onClick={this.clickHandler} className="calc-btn fifthRow calci">^</button>
         </section>
       </div>
     );
