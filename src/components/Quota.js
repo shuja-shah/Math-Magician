bf7b481033560aee37ffbb12a52079ba1a4d8287
shujashah83 <104627775+shujashah83@users.noreply.github.com>
2022-09-15 11:09:15 -0700

    Implement Requested ChangeS

--- src/components/Quota.js
+++ src/components/Quota.js
@@ -1,6 +1,11 @@
 const Quota = () => (
   <div className="Quota">
-    <h1>Quota</h1>
+    <h4>
+      Mathematics as an expression of the human mind reflects the active will,
+      the contemplative reason, and the desire for aesthetic perfection. Its bas
+      ic elements are logic and intuition, analysis and construction, generality and individuality.
+      <span className="Spani">— Richard Courant</span>
+    </h4>
   </div>
 );
 
