f1663e401dc2f074881a42dbbcd9cc1a8059b379
shujashah83 <104627775+shujashah83@users.noreply.github.com>
2022-09-15 06:01:16 -0700

    Develop App.js

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import Calculator from './components/calculator';
+
+const App = () => (
+  <Calculator />
+);
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Calculator from './components/calculator';
+import App from './App';
 import './index.css';
 
-ReactDOM.render(<Calculator />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
