ff4c98b4de053f8e7cd2139dfc9541e8784bafb0
shujashah83 <104627775+shujashah83@users.noreply.github.com>
2022-09-15 06:05:26 -0700

    Develop Nav Stateless Functional Componenet

---
+++ src/components/Navigation.js
@@ -0,0 +1,17 @@
+const Nav = () => (
+  <nav>
+    <ul>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/Calculate">Calculator</Link>
+      </li>
+      <li>
+        <Link to="/Qouta">Quota</Link>
+      </li>
+    </ul>
+  </nav>
+);
+
+export default Nav;
