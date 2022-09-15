f0a2a1e232b1eb9776ff5e5f4c566544be3330ea
shujashah83 <104627775+shujashah83@users.noreply.github.com>
2022-09-15 07:48:40 -0700

    Develop All Route Functionality

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,18 +1,10 @@
-import { NavLink } from 'react-router-dom';
-
 const Nav = () => (
   <nav className="navigationReal">
     <h2>Math Magician</h2>
     <ul>
-      <li>
-        <NavLink className="Navi" to="/">Home</NavLink>
-      </li>
-      <li>
-        <NavLink className="Navi" to="/Calculate">Calculator</NavLink>
-      </li>
-      <li>
-        <NavLink to="/Qouta">Quota</NavLink>
-      </li>
+      <li className="Navi">Home</li>
+      <li className="Navi">Calculator</li>
+      <li>Quota</li>
     </ul>
   </nav>
 );
